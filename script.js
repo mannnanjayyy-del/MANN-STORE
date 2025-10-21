406504d39cb02bedaef637556c9b69017527cfb4
mannnanjayyy-del <mannnanjayyy@gmail.com>
2025-10-21 13:24:52 +0700

    Update alert message on button click

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document .getElementById('sapaButton');
+const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click',function() {
-  alert('Halo terimakasi sudah berkunjung!');
+  alert('Woi tq udah berkunjung yaaaa!');
 });
